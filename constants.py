f7aa7e5a5e8eb7b4bfc8ccfcd1e843454ef99868
txoof <aaron.ciuffo@gmail.com>
2025-01-05 09:09:41 +0100

    chore: harmonize constant names

--- constants.py
+++ constants.py
@@ -2,39 +2,41 @@
 from pathlib import Path
 
 ### Paths
-CONFIG_PATH = Path(__file__).parent / "config"
-
-# Default Config File
-DEFAULT_CONFIG_FILE = CONFIG_PATH / "paperpi_config.yaml"
-
-# Application schema file
-APPLICATION_SCHEMA = CONFIG_PATH / "paperpi_config_schema.yaml"
-APPLICATION_SCHEMA_KEY = 'main'
-
-# Plugin manager schema
-PLUGIN_MANAGER_SCHEMA = CONFIG_PATH / "plugin_manager_schema.yaml"
+PATH_APP_CONFIG = Path(__file__).parent / "config"
+PATH_APP_PLUGINS = Path(__file__).parent / "plugins"
+PATH_DAEMON_CONFIG = Path("/etc/paperpi/")
+PATH_USER_CONFIG = Path("~/.config/com.txoof.paperpi/").expanduser().resolve()
 
 
-# Basic plugin schema
-PLUGIN_SCHEMA = CONFIG_PATH / "plugin_schema.yaml"
+
+# Files
+FNAME_PLUGIN_MANAGER_SCHEMA = "plugin_manager_schema.yaml"
+FNAME_PLUGIN_SCHEMA = "plugin_schema.yaml"
+FNAME_APPLICATION_SCHEMA = "paperpi_config_schema.yaml"
+
+FNAME_APPLICATION_CONFIG = "paperpi_config.yaml"
+FNAME_PLUGIN_CONFIG = "paperpi_plugins.yaml"
+
+# Dict keys
+KEY_APPLICATION_SCHEMA = 'main'
+KEY_PLUGIN_DICT = 'plugins'
 
 # PID for daemon mode
 PID_FILE = "/tmp/paperpi_daemon.pid"
 
-# Configuration file names
-APPLICATION_CONFIG = 'paperpi_config.yaml'
-PLUGIN_CONFIG = 'paperpi_plugins.yaml'
 
-# Location of user on-demand mode files
-CONFIG_ROOT_USER = Path("~/.config/com.txoof.paperpi")
-CONFIG_FILE_USER = CONFIG_ROOT_USER / APPLICATION_CONFIG
-PLUGIN_CONFIG_USER = CONFIG_ROOT_USER / PLUGIN_CONFIG
+# # Location of user on-demand mode files
+# CONFIG_ROOT_USER = Path("~/.config/com.txoof.paperpi")
+# CONFIG_FILE_USER = CONFIG_ROOT_USER / APPLICATION_CONFIG
+# PLUGIN_CONFIG_USER = CONFIG_ROOT_USER / PLUGIN_CONFIG
 
 
-# Location of daemon mode config files
-CONFIG_ROOT_DAEMON = Path("/etc/paperpi")
-CONFIG_FILE_DAEMON = CONFIG_ROOT_DAEMON / APPLICATION_CONFIG
-PLUGIN_CONFIG_DAEMON = CONFIG_ROOT_DAEMON
+# # Location of daemon mode config files
+# CONFIG_ROOT_DAEMON = Path("/etc/paperpi")
+# CONFIG_FILE_DAEMON = CONFIG_ROOT_DAEMON / APPLICATION_CONFIG
+# PLUGIN_CONFIG_DAEMON = CONFIG_ROOT_DAEMON
+
+
 
 
 ### Logging
