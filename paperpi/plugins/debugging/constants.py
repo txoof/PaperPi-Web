e340da2cf1f8b6ed797b7e3a6b460c4a8f24a151
txoof <aaron.ciuffo@gmail.com>
2025-08-18 17:12:39 +0200

    chore: update config files

--- paperpi/plugins/debugging/constants.py
+++ paperpi/plugins/debugging/constants.py
@@ -1,16 +1,8 @@
 version = '0.1.0'
 name = 'debugging'
 
-default_title = 'Debug'
-default_crash_rate = 0.5
+
+default_max_crash_rate = 0.5
 default_max_priority_rate = 0.2
-
-# do not include a sample INI configuration
-include_ini = False
-
-
-
-data = {
-    'title': 'title of plugin',
-    'crash_rate': 'float between 0 and 1 indicating how frequently plugin should crash'
-}
+default_max_sleep_time = 20
+default_title = f'Debug {default_max_crash_rate*100}pct'
